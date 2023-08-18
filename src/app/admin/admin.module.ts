6237837d219659bf87cbc930147f5e038480dc46
juliion <user@hp>
2023-08-18 14:27:04 +0300

    Add route guards

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { AddPageComponent } from "./add-page/add-page.component";
 import { OrdersPageComponent } from "./orders-page/orders-page.component";
 import { EditPageComponent } from "./edit-page/edit-page.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { authGuard } from "../common/auth.guard";
 
 @NgModule({
     declarations: [
@@ -28,10 +29,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
                 children: [
                     { path: '', redirectTo:'/admin/login',pathMatch:'full' },
                     { path: 'login', component: LoginPageComponent },
-                    { path: 'dashboard', component: DashboardPageComponent },
-                    { path: 'add', component: AddPageComponent },
-                    { path: 'orders', component: OrdersPageComponent },
-                    { path: 'product/:id/edit', component: EditPageComponent },
+                    { path: 'dashboard', component: DashboardPageComponent, canActivate:[ authGuard ]  },
+                    { path: 'add', component: AddPageComponent, canActivate:[ authGuard ]  },
+                    { path: 'orders', component: OrdersPageComponent, canActivate:[ authGuard ]  },
+                    { path: 'product/:id/edit', component: EditPageComponent, canActivate:[ authGuard ]  },
                 ]
             }
         ])
